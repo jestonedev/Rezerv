21f19b5ff4ceeb4e06f10b708b7d72b4d25043ad
IgnVV <ignvv@bratsk-city.ru>
2014-12-08 16:58:01 +0800

    Обновлен jQuery UI
    Signed-off-by: IgnVV <ignvv@bratsk-city.ru>

--- index.php
+++ index.php
@@ -7,8 +7,9 @@ header("Cache-Control: no-cache, must-revalidate");
         <meta charset="utf-8">
         <title>Заявки</title>
         <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-        <link href="css/redmond/jquery-ui-1.9.2.css" rel="stylesheet">
-        <link href="css/redmond/jquery-ui-1.9.2.theme.css" rel="stylesheet">
+        <link href="css/redmond/jquery-ui-1.11.2.min.css" rel="stylesheet">
+        <link href="css/redmond/jquery-ui-1.11.2.structure.min.css" rel="stylesheet">
+        <link href="css/redmond/jquery-ui-1.11.2.theme.min.css" rel="stylesheet">
         <link href="css/jquery.dataTables_1.10.4_themeroller.css" rel="stylesheet">
         <link href='css/jquery-ui-timepicker-addon.css' rel='stylesheet' type='text/css' />
         <link href="css/TableTools_JUI.css" rel="stylesheet">
@@ -17,7 +18,7 @@ header("Cache-Control: no-cache, must-revalidate");
         <link href='css/fullcalendar.print.css' rel='stylesheet' type='text/css'  media='print' />
 
         <script src="scripts/jquery-1.7.2.js" type='text/javascript'></script>
-        <script src="scripts/jquery-ui-1.9.2.custom.min.js" type='text/javascript'></script>
+        <script src="scripts/jquery-ui-1.11.2.min.js" type='text/javascript'></script>
         <script src="scripts/jquery.inputmask-3.1.0.js" type="text/javascript"></script>
         <script src="scripts/jquery.dataTables_1.10.4.min.js" type="text/javascript"></script>
         <script src='scripts/jquery-ui-timepicker-addon.js' type='text/javascript' ></script>
